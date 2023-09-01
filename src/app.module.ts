e6a43ee64995ade728d5ac5d8f44237eb9f4533f
Nikita Komov <komovn2000@gmail.com>
2023-09-01 16:19:25 +0300

    main: Added ConfigModule and TypeOrmModule

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,22 @@
 import { Module } from '@nestjs/common';
 import { CronJobModule } from './cron-job/cron-job.module';
 import { UserModule } from './user/user.module';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { DatabaseConfig } from './common/database/database.config';
 
 @Module({
-  imports: [CronJobModule, UserModule],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+    CronJobModule,
+    UserModule,
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      useClass: DatabaseConfig,
+      inject: [ConfigService],
+    }),
+  ],
 })
 export class AppModule {}
---
+++ src/common/database/database.config.ts
@@ -0,0 +1,39 @@
+import { ConfigService } from '@nestjs/config';
+import { join } from 'path';
+import { TypeOrmModuleOptions, TypeOrmOptionsFactory } from '@nestjs/typeorm';
+import { Inject } from '@nestjs/common';
+
+export class DatabaseConfig implements TypeOrmOptionsFactory {
+  constructor(
+    @Inject(ConfigService) private readonly configService: ConfigService,
+  ) {}
+
+  createTypeOrmOptions(): Promise<TypeOrmModuleOptions> | TypeOrmModuleOptions {
+    return {
+      type: 'postgres',
+      host: this.configService.get<string>(
+        'POSTGRES_HOST',
+        ' cr-test-database-1',
+      ),
+      port: this.configService.get<number>('POSTGRES_PORT', 5342),
+      username: this.configService.get<string>(
+        'POSTGRES_USER',
+        ' cr-test-database-1',
+      ),
+      password: this.configService.get<string>(
+        'POSTGRES_PASSWORD',
+        ' cr-test-database-1',
+      ),
+      database: this.configService.get<string>(
+        'POSTGRES_DB_NAME',
+        ' cr-test-database-1',
+      ),
+      entities: [join(__dirname, '..', '..', '**', '*.entity.{ts,js}')],
+      migrations: [
+        join(__dirname, '..', '..', 'migrations', '**', '*.{ts,js}'),
+      ],
+      synchronize: false,
+      logging: true,
+    };
+  }
+}
